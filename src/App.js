110aa700df46f0b3b59d50524756d6d9c39d4ab6
Павел Молоканов <pavelmolokanov@Fobos.local>
2024-03-26 11:26:51 -0700

    Add Navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
-import Navigation from './components/Navigation';
-import Hero from './components/Hero';
-import Highlights from './components/Highlights';
-import Testimonials from './components/Testimonials';
-import About from './components/About';
-import Footer from './components/Footer';
+import NavigationBar from './components/NavigationBar';
+// import Hero from './components/Hero';
+// import Highlights from './components/Highlights';
+// import Testimonials from './components/Testimonials';
+// import About from './components/About';
+// import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <Navigation />
-      <Hero />
+      <NavigationBar />
+      {/* <Hero />
       <Highlights />
       <Testimonials />
       <About />
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
