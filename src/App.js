e0678f163cecc93e4d0e89b38b984452f5648cc0
Павел Молоканов <pavelmolokanov@Fobos.local>
2024-03-30 17:49:12 -0700

    add Helmet for Meta data v.2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
+import HTMLComponent from './components/HTMLComponent';
 import './App.css';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
+
 
 function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <HTMLComponent />
     </>
   );
 }
